fa51be7359730d126f977bc4c1df30162ffb0a94
melp <huzi.master@outlook.com>
2024-03-15 07:51:16 +0800

    add: simple timer.

--- timing_trace_vite/src/App.jsx
+++ timing_trace_vite/src/App.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [started, setStarted] = useState(false);
   const [count, setCount] = useState(0);
+  const [elased, setElased] = useState("");
+
+  const initialDate = new Date();
+
+  const showTimer = (ms) => {
+    const seconds = Math.floor((ms / 1000) % 60)
+      .toString()
+      .padStart(2, "0");
+    const minutes = Math.floor((ms / 1000 / 60) % 60)
+      .toString()
+      .padStart(2, "0");
+    const hours = Math.floor((ms / 1000 / 60 / 60) % 60)
+      .toString()
+      .padStart(2, "0");
+
+    setElased(`${hours}:${minutes}:${seconds}`);
+  };
+
+  useEffect(() => {
+    if (started) {
+      const id = setInterval(() => {
+        var msecs = count + (new Date() - initialDate) ;
+        setCount(msecs);
+        showTimer(msecs);
+      }, 1000);
+      return () => clearInterval(id);
+    } else {
+      return;
+    }
+  }, [started]);
 
   return (
     <>
@@ -17,14 +46,25 @@ function App() {
             <p className="text-sm text-gray-400">Office Chores</p>
           </div>
 
-          <p className="text-center text-2xl mb-1 font-bold">00:21:13</p>
+          {/* <p className="text-center text-2xl mb-1 font-bold">00:21:13</p> */}
+          <p className="text-center text-2xl mb-1 font-bold">{elased}</p>
           <div className="flex justify-end pr-4 mb-2">
             <button className="mr-2">x</button>
             <button>edit</button>
           </div>
-          <button className="border w-full py-2 border-green-500 text-green-500 rounded-md">
-            start
-          </button>
+          {started ? (
+            <button className="border w-full py-2 border-red-500 text-red-500 rounded-b-md"
+                    onClick={() => setStarted(!started)}
+            >
+              stop
+            </button>
+          ) : (
+            <button className="border w-full py-2 border-green-500 text-green-500 rounded-b-md"
+                    onClick={() => setStarted(!started)}
+            >
+              start
+            </button>
+          )}
         </div>
       </div>
     </>
